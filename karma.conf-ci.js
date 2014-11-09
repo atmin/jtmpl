0773d7ffe1e32f5dabf4d786f422e2761f5522d9
Atanas Minev <fr33rider@gmail.com>
2014-11-09 22:07:06 +0200

    Travis.CI and SauceLabs integration

---
+++ karma.conf-ci.js
@@ -0,0 +1,101 @@
+var fs = require('fs');
+
+module.exports = function(config) {
+
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    if (!fs.existsSync('sauce.json')) {
+      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+      process.exit(1);
+    } else {
+      process.env.SAUCE_USERNAME = require('./sauce').username;
+      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+    }
+  }
+
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome'
+    },
+    'SL_Firefox': {
+      base: 'SauceLabs',
+      browserName: 'firefox'
+    },
+    'SL_IE9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '9'
+    },
+    'SL_IE10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '10'
+    },
+    'SL_IE11': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '11'
+    },
+    'SL_iPhone': {
+      base: 'SauceLabs',
+      browserName: 'iPhone',
+      platform: 'OSX 10.9',
+      version: '8.1'
+    }
+  };
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: [
+      'mocha',
+      'browserify'
+    ],
+
+    files: [
+      { pattern: 'src/**/*', included: false },
+      { pattern: 'spec/*', included: false },
+      'src/main.js',
+      'spec/main.js'
+    ],
+
+    preprocessors: {
+      'src/main.js': ['browserify'],
+      'spec/main.js': ['browserify']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['spec', 'saucelabs'],
+
+
+    // web server port
+    port: 9876,
+
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    sauceLabs: {
+      testName: 'jtmpl test suite'
+    },
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: Object.keys(customLaunchers),
+    singleRun: true
+  });
+};
